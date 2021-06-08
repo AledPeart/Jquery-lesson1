279aad8db3ebbfad2bb61f7b9d77a8ac7a47694b
AledPeart <aledpeart@gmail.com>
2021-06-08 07:08:46 +0000

    initial commit

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("tr:odd").addClass("odd");
+    $("tr:even").addClass("even");
+
+    $('h2').addClass("underline"); //underlines all h2 elements
+    $("ul").addClass("border"); //add border to all ul elements
+
+});
